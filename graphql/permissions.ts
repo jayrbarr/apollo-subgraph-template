6baf9dffb42ba64f88c3bd97da04d5ab2a8e9049
Jay Barr <jay.barr46@t-mobile.com>
2023-01-04 13:18:56 -0500

    Cleanup permissions

--- graphql/permissions.ts
+++ graphql/permissions.ts
@@ -1,4 +1,4 @@
-import { shield, rule, not, and, or } from 'graphql-shield';
+import { shield, rule, not, or } from 'graphql-shield';
 
 const isAuthenticated = rule({ cache: 'contextual' })(async (_, __, { user }) => {
     return user !== null
@@ -12,7 +12,6 @@ const isExpert = rule({ cache: 'contextual' })(async (_, __, { user }) => {
     return user.role === 'expert'
 })
 
-// Permissions
 export default shield({
     Query: {
         login: not(isAuthenticated),
